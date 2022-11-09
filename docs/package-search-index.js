54e3ac2f47613594306c7483df3a8d9e30cce9ab
韩帅 <shuai.han@ersoft.cn>
2022-11-09 18:04:05 +0800

    javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"cn.handyplus.lib.attribute"},{"l":"所有程序包","u":"allpackages-index.html"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cn.handyplus.lib.attribute","l":"AttributeEnum"},{"p":"cn.handyplus.lib.attribute","l":"AttributeUtil"}];updateSearchResults();
